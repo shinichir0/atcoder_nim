2ba4a7fa64d77b18326cee6784b0864b7fc0bf1d
Shin-ichiro Naito <18505311+shinichir0@users.noreply.github.com>
2018-05-19 05:06:15 +0900

    update

--- src/arc092a.nim
+++ src/arc092a.nim
@@ -1,12 +1,15 @@
 import strutils
 import sequtils
 import algorithm
+import queues
 
 
 type
   Point = tuple[x: int, y:int]
+  Node = tuple[blue: int, red: int]
 
 var point: Point
+var node: Node
 var red_points = newSeq[Point]()
 var blue_points = newSeq[Point]()
 let n = stdin.readLine.parseInt
@@ -32,34 +35,62 @@ proc cmp_point(a, b: Point): int =
     else:
       return 0
 
-proc cmp_graph(a, b: seq[int]): int =
-  if a.len > b.len:
-    return 1
-  elif a.len < b.len:
-    return -1
-  else:
-    return 0
-
 var num_pair = 0
 red_points.sort(cmp_point)
 blue_points.sort(cmp_point)
 
-var graph = newSeq[seq[int]]()
-
+var nodes = newSeq[Node]()
 for i, blue_point in blue_points:
-  var tmp = newSeq[int]()
   for j, red_point in red_points:
     if red_point.x < blue_point.x and red_point.y < blue_point.y:
-      tmp.add(j)
-  graph.add(tmp)
+      node = (i, j)
+      nodes.add(node)
 
-graph.sort(cmp_graph)
-var check = repeat(true, n)
-for nodes in graph:
-  for node in nodes:
-    if check[node]:
-      check[node] = false
-      num_pair.inc
-      break
-echo(graph)
+var visited = repeat(false, 2*n)
+
+echo(nodes)
+for i in 0 ..< n:
+  var color = "blue"
+  var flag = false
+  var now = i
+  var tmp_visited = initQueue[int]()
+  var previous = -1
+  while not flag:
+    echo(now)
+    flag = true
+    if color == "blue":
+      for node in nodes:
+        if node.blue == now:
+          if node.red + n in tmp_visited:
+            continue
+          if visited[node.red + n]:
+            color = "red"
+            flag = false
+            now = node.red
+            previous = -1
+            tmp_visited.add(node.blue)
+            break
+          else:
+            visited[i] = true
+            visited[node.red + n] = true
+            num_pair.inc
+            break
+    if color == "red":
+      for node in nodes:
+        if node.red == now:
+          if node.blue in tmp_visited:
+            continue
+          if visited[node.blue]:
+            color = "blue"
+            flag = false
+            now = node.blue
+            previous = -1
+            tmp_visited.add(node.red + n)
+            break
+          else:
+            continue
+      color = "blue"
+      flag = false
+      previous = now
+      now = tmp_visited.pop
 echo(num_pair)
